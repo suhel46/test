37caad19ee814926f9131294ec6a1848e4343911
Suhel Ahemed <Suhel.Ahemed@neom.com>
2023-05-04 11:09:41 +0300

    first commit

---
+++ src/main/java/c8y/example/Location.java
@@ -0,0 +1,44 @@
+package c8y.example;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Location {
+
+    private String city;
+    private String country_code;
+    private String latitude;
+    private String longitude;
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getCountry_code() {
+        return country_code;
+    }
+
+    public void setCountry_code(String country_code) {
+        this.country_code = country_code;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
